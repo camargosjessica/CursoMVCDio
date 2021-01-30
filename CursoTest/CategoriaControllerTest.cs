745067328ece5eb531c9284030ce53f5483e4f96
camargosjessica <camargosjessica@hotmail.com>
2021-01-29 21:16:54 -0300

    adicionando classe de testes

---
+++ CursoTest/CategoriaControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using CursoAPI.Controllers;
+using CursoMVCDio.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CursoTest
+{
+    public class CategoriasControllerTest
+     {
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Categoria _categoria;
+        public CategoriasControllerTest()
+        {
+            _mockSet = new Mock<DbSet<Categoria>>();
+            _mockContext = new Mock<Context>();
+            _categoria = new Categoria { Id = 1, Descricao = "Teste Categoria" };
+        }
+
+        [Fact]
+        public async Task Get_Categoria()
+        {
+            var service = new CategoriasController(_mockContext.Object);
+
+            await service.GetCategoria(1);
+
+            _mockSet.Verify(m => m.FindAsync(1),
+                Times.Once());
+        }
+    }
+}
